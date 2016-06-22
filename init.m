4e78010233bac0fd4d0986c87841d5998bf45485
David Bindel <bindel@cs.cornell.edu>
2016-06-22 11:56:59 -0400

    Moved networkComponents.m to third_party.

--- init.m
+++ init.m
@@ -3,4 +3,5 @@
 addpath(fullfile(pwd, 'src'));
 addpath(fullfile(pwd, 'data'));
 addpath(fullfile(pwd, 'test'));
+addpath(fullfile(pwd, 'third_party'));
 addpath(fullfile(pwd, 'third_party', 'UFget'));
